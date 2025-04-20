6002a1001c516187cb20618508005bc55f847efb
kingnavneeth094 <ksnavneeth614@gmail.com>
2025-04-20 23:47:41 +0530

    Added meta data info

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,12 @@ import "./globals.css";
 import { ThemeProvider } from "../components/ThemeContext";
 import { Analytics } from "@vercel/analytics/next";
 
+export const metadata = {
+  title: "Navneeth's Portfolio",
+  description: "Welcome to Navneeth's personal portfolio. Explore projects, skills, and connect via social media!",
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
